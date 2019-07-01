b345ff64bc144bbf36b964a64b73b0bc1e818f6b
Pradipkumar Hinge <hingepradeepster@gmail.com>
2019-07-01 20:22:19 +0530

    Initial commit for Our first project mpscstarter

---
+++ src/main/java/com/mpscstarter/MpscstarterApplication.java
@@ -0,0 +1,13 @@
+package com.mpscstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpscstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpscstarterApplication.class, args);
+	}
+
+}
