4cb89d398e1b9ce4910d30aee43db854eedcbea5
Pradipkumar Hinge <hingepradeepster@gmail.com>
2019-07-02 19:10:55 +0530

    Added bootstrap powered index file

--- src/main/java/com/mpscstarter/controllers/IndexController.java
+++ src/main/java/com/mpscstarter/controllers/IndexController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloWorldController {
-
-	@RequestMapping("/")	
-	public String sayHello() {
+public class IndexController {
+	@RequestMapping("/")
+	public String showHome() {
 		return "index";
 	}
-	
 }
